ba450bc70971c63f3908364521297842707ef36f
khajornkiat <khajornkiat.us@ols.co.th>
2021-05-11 10:09:39 +0700

    [edit] change to firebase functions

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const Telegraf = require("telegraf");
+const functions = require("firebase-functions");
+
+const bot = new Telegraf(functions.config().telegrambot.key);
+
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
+
+exports.bot = functions.https.onRequest((req, res) => {
+  bot.handleUpdate(req.body, res);
+});
