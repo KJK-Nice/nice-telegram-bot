b757f5aaf179babe4b1c94cd5525f17956650a59
khajornkiat <khajornkiat.us@ols.co.th>
2021-05-11 22:34:01 +0700

    [edit] add handle error. and echo bot

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,26 @@
 const functions = require("firebase-functions");
 const {Telegraf} = require("telegraf");
 
-const bot = new Telegraf(functions.config().telegrambot.key);
+const bot = new Telegraf(functions.config().telegrambot.key, {
+  telegram: {webhookReply: true},
+});
 
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.launch();
+// Error handling
+bot.catch((err, ctx) => {
+  functions.logger.error("[Bot] Error", err)
+  return ctx.reply(`Ooop, encountered an error for ${ctx.updateType}`, err)
+});
 
-exports.bot = functions.https.onRequest((req, res) => {
-  bot.handleUpdate(req.body, res);
+// initialize the commands
+bot.command("/start", (ctx) => ctx.reply("Hello! Send any message and I will copy it."));
+// copy every message and send to the user
+bot.on("message", (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
+
+// Handle all telegram updates with HTTPs trigger
+exports.echoBot = functions.https.onRequest(async (req, res) => {
+  functions.logger.log("Incoming message", req.body)
+  return await bot.handleUpdate(req.body, res).then((rv) => {
+    // If it's not a request form the telegram, rv will be undefined, but we should respond with 200
+    return !rv && res.sendStatus(200)
+  })
 });
