c6087e91e87f6adaa531779d2b9c67658d359d3c
khajornkiat <khajornkiat.us@ols.co.th>
2021-05-12 09:00:08 +0700

    [edit] add error handling

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,26 @@
 const functions = require("firebase-functions");
-const {Telegraf} = require("telegraf");
+const {
+  Telegraf,
+} = require("telegraf");
 
-const bot = new Telegraf(functions.config().telegrambot.key);
+const bot = new Telegraf(functions.config().telegrambot.key, {
+  telegram: {
+    webhookReply: true,
+  },
+});
 
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.launch();
+// Error handling
+bot.catch((err, ctx) => {
+  functions.logger.error("[Bot] Error", err);
+  return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err);
+});
+
+// Initialize the commands
+bot.command("/start", (ctx) => ctx.reply(
+    "Hello! Send any message and I will copy it."
+));
+// Copy every message and send to the user
+bot.on("message", (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
 
 exports.bot = functions.https.onRequest((req, res) => {
   bot.handleUpdate(req.body, res);
